35fa946210dc523838c8e23bbb4f840608ad1329
Angel Isaac <angel.garcia@devitm.com>
2025-05-05 12:38:25 -0600

    158- Validando el Formulario

--- 19-PROYECTO-LocalStorage/js/app.js
+++ 19-PROYECTO-LocalStorage/js/app.js
@@ -12,10 +12,36 @@ function eventListeners() {
 
 
 
-
-// Funciones
+ // Funciones
 function agregarTweet(e) {
     e.preventDefault();
 
-    console.log('Agregando tweet');
+    //Textarea donde el usuario escribe
+    const tweet = document.querySelector('#tweet').value;
+
+    //validacion
+     if (tweet === '') {
+       mostrarError('UN mensaje no puede ir vacio');
+
+        return; //evita que se ejecuten mas lineas de codigo
+     }
+
+     console.log('agregando tweet');
+}
+
+
+// Mostrar mensaje de error
+function mostrarError(error) {
+    const mensajeError = document.createElement('p');
+    mensajeError.textContent = error;
+    mensajeError.classList.add('error');
+
+    // insertarlo en el contenido
+    const contenido = document.querySelector('#contenido');
+    contenido.appendChild(mensajeError);
+
+    //Elimina la alerta despues de 2 segundos
+    setTimeout(()=>{
+        mensajeError.remove();
+    }, 2000);
 }
