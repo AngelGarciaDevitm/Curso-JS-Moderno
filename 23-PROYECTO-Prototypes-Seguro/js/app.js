a112148bd39baf1acbd5867d44846536629b490e
Angel Isaac <angel.garcia@devitm.com>
2025-05-07 11:48:32 -0600

    172- Validar el formulario

--- 23-PROYECTO-Prototypes-Seguro/js/app.js
+++ 23-PROYECTO-Prototypes-Seguro/js/app.js
@@ -29,3 +29,30 @@ const ui = new UI();
 document.addEventListener('DOMContentLoaded', () => {
     ui.llenarOpciones();// Llena el select de los años...
 })
+
+
+eventListeners();
+function eventListeners() {
+    const formulario = document.querySelector('#cotizar-seguro');
+    formulario.addEventListener('submit', cotizarSeguro);
+}
+
+function cotizarSeguro(e) {
+    e.preventDefault();
+
+    //leer la marca seleccionada 
+    const marca = document.querySelector('#marca').value;
+
+    //leer el año seleccionado
+    const year = document.querySelector('#year').value;
+
+    //leer el tipo de cobertura
+    const tipo = document.querySelector('input[name="tipo"]:checked').value;
+    
+    if(marca === '' || year === '' || tipo === '') {
+        console.log('no valido');
+    } else {
+        console.log('si valido');
+    }
+
+}
