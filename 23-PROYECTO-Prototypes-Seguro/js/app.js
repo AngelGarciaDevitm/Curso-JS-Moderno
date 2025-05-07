3de7ee31c62fac82fcbedddc8a2d947435425127
Angel Isaac <angel.garcia@devitm.com>
2025-05-07 15:16:01 -0600

    175- Mostrar el Total a pagar

--- 23-PROYECTO-Prototypes-Seguro/js/app.js
+++ 23-PROYECTO-Prototypes-Seguro/js/app.js
@@ -88,7 +88,52 @@ UI.prototype.mostrarMensaje = (mensaje, tipo) => {
 
     setTimeout (() => {
         div.remove();
-    }, 3000);
+    }, 2000);
+}
+
+UI.prototype.mostrarResultado = (total, seguro) => {
+
+    const { marca, year, tipo } = seguro;
+
+    let textoMarca;
+
+    switch(marca) {
+        case '1':
+            textoMarca = 'Americano';
+            break;
+        case '2':   
+            textoMarca = 'Asiatico';
+            break;
+        case '3':
+            textoMarca = 'Europeo';
+            break;
+        default:
+            break;
+    }
+
+    // Crear el resultado
+    const div = document.createElement('div');
+    div.classList.add('mt-10');
+
+    div.innerHTML = `
+        <p class="header">Tu Resumen</p>
+        <p class="font-bold">Marca: <span class="font-normal"> ${textoMarca}</span></p>
+        <p class="font-bold">Año: <span class="font-normal"> ${year}</span></p>
+        <p class="font-bold">Tipo de seguro: <span class="font-normal capitalize"> ${tipo}</span></p>
+        <p class="font-bold">Total: <span class="font-normal"> $ ${total}</span></p>
+    `;
+
+    const resultadoDiv = document.querySelector('#resultado');
+    
+
+    // Mostrar spinner
+    const spinner = document.querySelector('#cargando');
+    spinner.style.display = 'block';
+
+    setTimeout(() => {
+        spinner.style.display = 'none'; // se borra el spinner
+        resultadoDiv.appendChild(div); // se muestra el resultado
+    }, 2000);
 }
 
 // Instanciar UI 
@@ -125,10 +170,17 @@ function cotizarSeguro(e) {
 
     ui.mostrarMensaje('Cotizando...','correcto');
 
+    //ocultar las cotizaciones previas
+    const resultados = document.querySelector('#resultado div');
+    if(resultados != null) {
+        resultados.remove();
+    }
+
     // Instanciar el seguro
     const seguro = new Seguro(marca, year, tipo);
-    seguro.cotizarSeguro();
+    const total = seguro.cotizarSeguro();
 
     // Utilizar el prototype que va a cotizar
+    ui.mostrarResultado(total, seguro);
 
 }
