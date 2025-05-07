208d547cd1fd7bf001ea9d583b5a1d284245d906
Angel Isaac <angel.garcia@devitm.com>
2025-05-07 12:05:47 -0600

    173- Crear un prototype para mostrar un error

--- 23-PROYECTO-Prototypes-Seguro/js/app.js
+++ 23-PROYECTO-Prototypes-Seguro/js/app.js
@@ -22,6 +22,29 @@ UI.prototype.llenarOpciones = () => {
     }
 }
 
+// Muestra alertas en pantalla
+UI.prototype.mostrarMensaje = (mensaje, tipo) => {
+
+    const div = document.createElement('div');
+
+    if(tipo === 'error') {
+        div.classList.add('error');
+    } else {
+        div.classList.add('correcto');
+    }
+
+    div.classList.add('mensaje', 'mt-10');
+    div.textContent = mensaje;
+
+    // Insertar en el HTML 
+    const formulario = document.querySelector('#cotizar-seguro');
+    formulario.insertBefore(div, document.querySelector('#resultado'));
+
+    setTimeout (() => {
+        div.remove();
+    }, 2000);
+}
+
 // Instanciar UI 
 const ui = new UI();
 
@@ -50,9 +73,10 @@ function cotizarSeguro(e) {
     const tipo = document.querySelector('input[name="tipo"]:checked').value;
     
     if(marca === '' || year === '' || tipo === '') {
-        console.log('no valido');
-    } else {
-        console.log('si valido');
-    }
+        ui.mostrarMensaje('Todos los campos son obligatorios', 'error');
+        return;
+    } 
+
+    ui.mostrarMensaje('Cotizando...','correcto');
 
 }
