4192fbf50cf45e1e754d93cf492817fcd0cc2aa2
Angel Isaac <angel.garcia@devitm.com>
2025-05-12 09:53:11 -0600

    190- Comenzando el proyecto

---
+++ 26-PROYECTO-AdministrarCitas/js/app.js
@@ -0,0 +1,7 @@
+//selectores 
+const pacienteInput = document.querySelector('#paciente');
+const propietarioINput = document.querySelector('#propietario');
+const emailInput = document.querySelector('#email');
+const fechaInput = document.querySelector('#fecha');
+const sintomasInput = document.querySelector('#sintomas');
+
