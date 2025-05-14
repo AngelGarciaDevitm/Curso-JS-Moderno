0ec0a16819cb22e40fe3d2365125287a797405db
Angel Isaac <angel.garcia@devitm.com>
2025-05-14 11:24:26 -0600

    206- Primeros pasos con el boton editar

--- 26-PROYECTO-AdministrarCitas/js/app.js
+++ 26-PROYECTO-AdministrarCitas/js/app.js
@@ -10,6 +10,12 @@ const formulario = document.querySelector('#nueva-cita')
 
 const contenedor = document.querySelector('#administra');
 
+const btnEditar = document.querySelector('.btn-editar');
+
+btnEditar?.addEventListener('click', () => {
+    alert('diste click');
+})
+
 // Eventos
 mascotaInput.addEventListener('change', datosCita);
 propietarioInput.addEventListener('change', datosCita);
@@ -117,9 +123,10 @@ class Citas {
 
             // Añade un botón de editar...
             const btnEditar = document.createElement('button');
-            btnEditar.onclick = () => cargarEdicion(cita);
-            
-            btnEditar.classList.add('btn', 'btn-info');
+            btnEditar.onclick = function () {
+                alert('diste click');
+            }
+            btnEditar.classList.add('btn', 'btn-info', 'btn-editar');
             btnEditar.innerHTML = 'Editar <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"></path></svg>'
 
             const contenedorBotones = document.createElement('DIV');
