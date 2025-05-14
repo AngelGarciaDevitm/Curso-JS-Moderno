315610dcca9172cbfced1a5f280b2e46f0e3db60
Angel Isaac <angel.garcia@devitm.com>
2025-05-14 11:32:26 -0600

    207- Añadir la informacion al objeto de cita que deseamos editar

--- 26-PROYECTO-AdministrarCitas/js/app.js
+++ 26-PROYECTO-AdministrarCitas/js/app.js
@@ -123,9 +123,7 @@ class Citas {
 
             // Añade un botón de editar...
             const btnEditar = document.createElement('button');
-            btnEditar.onclick = function () {
-                alert('diste click');
-            }
+            btnEditar.onclick = () => cargarEdicion(cita);
             btnEditar.classList.add('btn', 'btn-info', 'btn-editar');
             btnEditar.innerHTML = 'Editar <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"></path></svg>'
 
@@ -195,4 +193,8 @@ function reiniciarObjeto() {
         telefono: '',
         sintomas: ''
     })
+}
+
+function cargarEdicion (cita) {
+    console.log(cita);
 }
