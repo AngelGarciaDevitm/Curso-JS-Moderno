9126a2c26513b6701685de481b3fcb641ccf1df3
Angel Isaac <angel.garcia@devitm.com>
2025-05-14 11:15:45 -0600

    205- Agregando botones para editar y eliminar

--- 26-PROYECTO-AdministrarCitas/js/app.js
+++ 26-PROYECTO-AdministrarCitas/js/app.js
@@ -108,7 +108,27 @@ class Citas {
             const sintomas = document.createElement('p');
             sintomas.classList.add('font-normal', 'mb-3', 'text-gray-700', 'normal-case')
             sintomas.innerHTML = `<span class="font-weight-bolder">Sintomas: </span> ${cita.sintomas}`;
-        
+
+            // Agregar un botón de eliminar...
+            const btnEliminar = document.createElement('button');
+            btnEliminar.onclick = () => eliminarCita(id); // añade la opción de eliminar
+            btnEliminar.classList.add('btn', 'btn-danger', 'mr-2');
+            btnEliminar.innerHTML = 'Eliminar <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>'
+
+            // Añade un botón de editar...
+            const btnEditar = document.createElement('button');
+            btnEditar.onclick = () => cargarEdicion(cita);
+            
+            btnEditar.classList.add('btn', 'btn-info');
+            btnEditar.innerHTML = 'Editar <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"></path></svg>'
+
+            const contenedorBotones = document.createElement('DIV');
+            contenedorBotones.classList.add('flex', 'justify-between', 'mt-10')
+
+            contenedorBotones.appendChild(btnEditar);
+            contenedorBotones.appendChild(btnEliminar);
+
+
             // Agregar al HTML
             divCita.appendChild(paciente);
             divCita.appendChild(propietario);
@@ -116,6 +136,7 @@ class Citas {
             divCita.appendChild(hora);
             divCita.appendChild(fecha);
             divCita.appendChild(sintomas);
+            divCita.appendChild(contenedorBotones);
             contenedor.appendChild(divCita);
         });    
     }
